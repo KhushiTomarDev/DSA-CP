ee6093c8816f542b45c3a8007b62d9c31f717139
Khushi <khushitomar86@gmail.com>
2025-07-21 21:03:41 +0530

    Solved Leetcode problems: Missing Number & Contains Duplicate

---
+++ _01Arrays/Easy/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/contains-duplicate/solutions/
+
+package _01Arrays.Easy;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums={2,3,4,5};
+        System.out.println(containsDuplicate(nums));
+
+    }
+}
---
+++ _01Arrays/Easy/MissingNumber.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/missing-number/
+
+package _01Arrays.Easy;
+
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int  n=nums.length;
+        int res =n;
+        for (int i=0;i<n;i++){
+            res+=i-nums[i];
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,0,3,4};
+        System.out.println(missingNumber(nums));
+    }
+}
