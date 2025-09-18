47d4d14a0eaf3d05d0e8fd817bf946c9736c4fd2
Khushi <khushitomar86@gmail.com>
2025-09-18 21:46:55 +0530

    Added Question

---
+++ _01Arrays/Medium/FindTheDuplicateNumber.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/find-the-duplicate-number/description/
+
+package _01Arrays.Medium;
+
+public class FindTheDuplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        int slow2 = nums[0];
+
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;
+    }
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new int[]{3,4,5,3,7,6}));
+    }
+}
