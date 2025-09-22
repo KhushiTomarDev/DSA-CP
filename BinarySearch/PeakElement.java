cbb63ce28b8ac2171755ed8e2af12d0f1b87fa79
Khushi <khushitomar86@gmail.com>
2025-09-22 13:46:46 +0530

    Added PeakElement Question

---
+++ BinarySearch/PeakElement.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/find-peak-element/description/
+
+package BinarySearch;
+
+public class PeakElement {
+    public static int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+    public static void main(String[] args) {
+        System.out.println(findPeakElement(new int[]{2,3,4,5,4,5,8,4}));
+    }
+    
+}
