ba01990a9331ff95bcde823efd6de10d07982a32
Khushi <khushitomar86@gmail.com>
2025-09-23 21:44:23 +0530

    Question added

---
+++ BinarySearch/PeakElementInAMountainArray.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+package BinarySearch;
+
+public class PeakElementInAMountainArray {
+    public int peakIndexInMountainArray(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
