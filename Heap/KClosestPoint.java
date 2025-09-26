7d5556e92e643ce5d5e0f158c8cefc64ad4709ce
Khushi <khushitomar86@gmail.com>
2025-09-26 21:13:02 +0530

    Added Heap Folder

---
+++ Heap/KClosestPoint.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/k-closest-points-to-origin/description/
+
+
+import java.util.Arrays;
+
+public class KClosestPoint {
+    public static int[][] kClosest(int[][] points, int k) {
+
+
+         Arrays.sort(points, (a, b) -> 
+            (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])
+        );
+        return Arrays.copyOfRange(points, 0, k);
+
+}
+public static void main(String[] args) {
+    int[][] points = {{1, 3}, {-2, 2}};
+    int k = 1;
+    System.out.println(Arrays.deepToString(kClosest(points, k)));
+}
+    
+}
