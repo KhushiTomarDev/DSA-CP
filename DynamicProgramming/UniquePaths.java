3b1c8e38aafcd35c69136fda9ea88fb36ac4ef96
Khushi <khushitomar86@gmail.com>
2025-10-03 18:44:51 +0530

    Added A Que

---
+++ DynamicProgramming/UniquePaths.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/unique-paths/description/
+
+
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+    public static int uniquePaths(int m, int n) {
+        int[] aboveRow = new int[n];
+        Arrays.fill(aboveRow, 1);
+
+        for (int row = 1; row < m; row++) {
+            int[] currentRow = new int[n];
+            Arrays.fill(currentRow, 1);
+            for (int col = 1; col < n; col++) {
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+            aboveRow = currentRow;
+        }
+
+        return aboveRow[n - 1];        
+    }
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(6, 0x7));
+    }
+
+}
