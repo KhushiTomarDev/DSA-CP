4dd554c36f676cbc27d73f0ec0914fbfa2c7f413
Khushi <khushitomar86@gmail.com>
2025-10-12 20:52:54 +0530

    Added a que

---
+++ DynamicProgramming/TargetSum.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/target-sum/description/
+
+package DynamicProgramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TargetSum {
+    
+    public static int findTargetSumWays(int[] nums, int target) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        counter.put(0, 1);
+
+        for (int n : nums) {
+            Map<Integer, Integer> temp = new HashMap<>();
+
+            for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+                int total = entry.getKey();
+                int count = entry.getValue();
+
+                temp.put(total + n, temp.getOrDefault(total + n, 0) + count);
+                temp.put(total - n, temp.getOrDefault(total - n, 0) + count);
+            }
+            counter = temp;
+        }
+
+        return counter.getOrDefault(target, 0);        
+    }
+
+}
