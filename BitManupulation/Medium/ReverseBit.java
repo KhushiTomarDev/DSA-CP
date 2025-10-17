ed0961896f69a2c46a02bfe6ecc97d3761785d85
Khushi <khushitomar86@gmail.com>
2025-10-17 21:54:16 +0530

    Added a question

---
+++ BitManupulation/Medium/ReverseBit.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/reverse-bits/description/
+
+
+public class ReverseBit {
+
+    public static int reverseBits(int n) {
+
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+
+            result |= n & 1;
+
+            n >>>= 1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseBits(43261596));
+    }
+
+}
