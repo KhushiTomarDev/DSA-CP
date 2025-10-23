b2fa48197d8b1eeecaf1a9676481d72c6bd0ed0a
Khushi <khushitomar86@gmail.com>
2025-10-23 20:56:57 +0530

    Added A que

---
+++ Greedy/JumpGame2.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+public class JumpGame2 {
+    public static int jump(int[] nums) {
+        int near = 0, far = 0, jumps = 0;
+
+        while (far < nums.length - 1) {
+            int farthest = 0;
+            for (int i = near; i <= far; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;        
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,6,7};
+        System.out.println(jump(nums));
+    }
+
+}
