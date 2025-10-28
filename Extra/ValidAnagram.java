1b846db8e7257e71c3c5eb2ba7c77abe4483417f
Khushi <khushitomar86@gmail.com>
2025-10-28 23:17:58 +0530

    Added a que

---
+++ Extra/ValidAnagram.java
@@ -0,0 +1,34 @@
+package Extra;
+
+//
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> counter = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
+                return false;
+            }
+            counter.put(ch, counter.get(ch) - 1);
+        }
+
+        return true;        
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("abser", "RTESF"));
+    }
+}
