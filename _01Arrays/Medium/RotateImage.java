6ec1ea4ca66f6375870097c4af73a9311bb2430c
Khushi <khushitomar86@gmail.com>
2025-11-08 21:58:38 +0530

    Added a Que

---
+++ _01Arrays/Medium/RotateImage.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/rotate-image/description/
+
+package _01Arrays.Medium;
+
+import java.util.Arrays;
+
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        int edgeLength = matrix.length;
+
+        int top = 0;
+        int bottom = edgeLength - 1;
+
+        while (top < bottom) {
+            for (int col = 0; col < edgeLength; col++) {
+                int temp = matrix[top][col];
+                matrix[top][col] = matrix[bottom][col];
+                matrix[bottom][col] = temp;
+            }
+            top++;
+            bottom--;
+        }
+
+        for (int row = 0; row < edgeLength; row++) {
+            for (int col = row + 1; col < edgeLength; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }        
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+}
