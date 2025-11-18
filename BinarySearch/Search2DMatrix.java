848fa9ae35f07de8358e170f804b3b3b51e6e9cf
Khushi <khushitomar86@gmail.com>
2025-11-18 21:59:38 +0530

    updated solution

--- BinarySearch/Search2DMatrix.java
+++ BinarySearch/Search2DMatrix.java
@@ -1,7 +1,7 @@
 //https://leetcode.com/problems/search-a-2d-matrix/description/
 
 public class Search2DMatrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] matrix, int target) {
         int top = 0;
         int bot = matrix.length - 1;
 
@@ -35,6 +35,10 @@ public class Search2DMatrix {
         }
 
         return false;        
+    }
+    public static void main(String[] args) {
+        int[][] nums = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        System.out.println(searchMatrix(nums, 7));
     }
 
 }
