4430e4ce2cf63a68f7af1e7c458d06e19cda5207
Khushi <khushitomar86@gmail.com>
2025-11-22 22:53:55 +0530

    Updated test case

--- DynamicProgramming/CoinChange.java
+++ DynamicProgramming/CoinChange.java
@@ -24,6 +24,7 @@ public class CoinChange {
 
     public static void main(String[] args) {
         System.out.println(coinChange(new int[]{2, 3, 4, 5, 6}, 10));
+        System.out.println(coinChange(new int[]{2, 3, 4, 5, 6}, 20));
     }
 
 }
