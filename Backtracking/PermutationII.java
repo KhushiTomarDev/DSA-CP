f68e8934ab5614b62a4d331d17c946f0dfaaf12c
Khushi <khushitomar86@gmail.com>
2025-11-23 22:34:22 +0530

    Added a question

---
+++ Backtracking/PermutationII.java
@@ -0,0 +1,44 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PermutationII {
+    class Solution {
+    public void backtrack(int i, int[] nums, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int x : nums) temp.add(x);
+            res.add(temp);
+            return;
+        }
+
+        Set<Integer> seen = new HashSet<>();
+
+        for (int j = i; j < nums.length; j++) {
+            if (seen.contains(nums[j])) continue;
+
+            seen.add(nums[j]);
+
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+
+            backtrack(i + 1, nums, res);
+
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp; // backtrack
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res);
+        return res;
+    }
+}
+    
+}
