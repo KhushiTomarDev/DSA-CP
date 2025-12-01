d2829ed4e939aff535af34d3e9755694212b474e
Khushi <khushitomar86@gmail.com>
2025-12-01 19:53:53 +0530

    Updated solution

--- DynamicProgramming/WordBreak.java
+++ DynamicProgramming/WordBreak.java
@@ -1,7 +1,24 @@
 //https://leetcode.com/problems/word-break/description/
 
 package DynamicProgramming;
-
 public class WordBreak {
-    
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (String w : wordDict) {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];        
+    }
+    public static void main(String[] args) {
+        
+    }
+
 }
