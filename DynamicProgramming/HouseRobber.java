13f2134b7f219f9d7b5f206e21d88806be75073d
Khushi <khushitomar86@gmail.com>
2025-12-05 20:43:40 +0530

    Updated solution

--- DynamicProgramming/HouseRobber.java
+++ DynamicProgramming/HouseRobber.java
@@ -17,7 +17,9 @@ public class HouseRobber {
     }
     public static void main(String[] args) {
         int[] nums= {3,4,5,6,7};
+        int[] nums1= {3,4,5,6,9};
         System.out.println(rob(nums));
+        System.out.println(rob(nums1));
     }
 
 }
