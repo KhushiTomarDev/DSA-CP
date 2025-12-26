d4ba28d68b0ee25751748e09f8ef120a7607184f
Khushi <khushitomar86@gmail.com>
2025-12-26 19:49:41 +0530

    Added +1 Solution for a question

--- _01Arrays/Medium/FindAllDuplicate.java
+++ _01Arrays/Medium/FindAllDuplicate.java
@@ -3,25 +3,44 @@
 package _01Arrays.Medium;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class FindAllDuplicate {
-       public static List<Integer> findDuplicates(int[] nums) {
-        Set<Integer> hs=new HashSet<>();
-        List<Integer> ans=new ArrayList<>();
-        for(int x:nums){
-            boolean f=hs.add(x);
-            if(!f)
-                ans.add(x);
+    //    public static List<Integer> findDuplicates(int[] nums) {
+    //     Set<Integer> hs=new HashSet<>();
+    //     List<Integer> ans=new ArrayList<>();
+    //     for(int x:nums){
+    //         boolean f=hs.add(x);
+    //         if(!f)
+    //             ans.add(x);
+    //     }
+    //     return ans;
+    // }
+    // public static void main(String[] args) {
+    //     System.out.println(findDuplicates(new int[]{2,3,4,2,4,5,6,7,4,3,2}));
+    //     System.out.println(findDuplicates(new int[]{   4,5,6,7,4,3,2}));
+    //     System.out.println(findDuplicates(new int[]{2,3,4,2,4,7,4,3,2}));
+    // }
+
+    
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            int idx = num - 1;
+            if (nums[idx] < 0)
+                result.add(num);
+            nums[idx] *= -1;
         }
-        return ans;
+        return result;
     }
+
     public static void main(String[] args) {
-        System.out.println(findDuplicates(new int[]{2,3,4,2,4,5,6,7,4,3,2}));
-        System.out.println(findDuplicates(new int[]{   4,5,6,7,4,3,2}));
-        System.out.println(findDuplicates(new int[]{2,3,4,2,4,7,4,3,2}));
+        int[] nums = {1,2,1,3,4,3,5};
+        System.out.println(findDuplicates(nums));
     }
+
+
     
 }
