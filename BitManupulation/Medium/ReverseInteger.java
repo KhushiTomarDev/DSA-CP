6ed7dcc3b16be45148c73f510ce144aff5f501b6
Khushi <khushitomar86@gmail.com>
2025-12-30 22:30:21 +0530

    Added another method

--- BitManupulation/Medium/ReverseInteger.java
+++ BitManupulation/Medium/ReverseInteger.java
@@ -4,20 +4,41 @@ package BitManupulation.Medium;
 
 public class ReverseInteger {
 
+    // public static int reverse(int x) {
+    //     int res = 0;
+    //     boolean isNegative = x < 0;
+    //     String strX = String.valueOf(Math.abs(x));
+    //     StringBuilder sb = new StringBuilder(strX).reverse();
+        
+    //     try {
+    //         res = Integer.parseInt(sb.toString());
+    //     } catch (NumberFormatException e) {
+    //         return 0;
+    //     }
+        
+    //     return isNegative ? -res : res;    
+    
+    //------------Method 2-----------------------------
     public static int reverse(int x) {
-        int res = 0;
-        boolean isNegative = x < 0;
-        String strX = String.valueOf(Math.abs(x));
-        StringBuilder sb = new StringBuilder(strX).reverse();
-        
-        try {
-            res = Integer.parseInt(sb.toString());
-        } catch (NumberFormatException e) {
-            return 0;
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+            x = -x;
         }
-        
-        return isNegative ? -res : res;       
+
+        int res = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (res > (Integer.MAX_VALUE - digit) / 10) {
+                return 0;
+            }
+            res = (res * 10) + digit;
+        }
+
+        return isNegative ? -res : res;
     }
+
     public static void main(String[] args) {
         System.out.println(reverse(87));
     }
