2ce9bc1f2bf23ee8f5904a88c728305c65e6c3ae
Khushi <khushitomar86@gmail.com>
2026-01-13 13:48:16 +0530

    Added a question

---
+++ Heap/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthSmallestElementInSortedMatrix {
+     public static int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> nums = new PriorityQueue<>(Collections.reverseOrder());
+        //max heap
+        for(int[] row : matrix){
+            for(int val : row){
+                nums.offer(val);
+                if(nums.size()>k){
+                    nums.poll();
+                }
+
+            }
+        }
+            return nums.peek();
+    }
+    public static void main(String[] args) {
+        int[][] nums = {{2,3},{4,5}};
+        System.out.println(kthSmallest(nums, 2));
+    }
+}
