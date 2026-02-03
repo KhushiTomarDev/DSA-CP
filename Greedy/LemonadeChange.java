525e3f8e668b30e21ff50836975f7a47ba100d32
Khushi <khushitomar86@gmail.com>
2026-02-03 22:37:13 +0530

    Updated

--- Greedy/LemonadeChange.java
+++ Greedy/LemonadeChange.java
@@ -33,6 +33,8 @@ public class LemonadeChange {
         }
     public static void main(String[] args) {
         int[] bills = {2,3,45,66};
-        System.out.println(lemonadeChange(bills));
+        int[] bills1 = {45,66};
     }
+    System.out.println(lemonadeChange(bills));
+    System.out.println(lemonadeChange(bills1));
 }
